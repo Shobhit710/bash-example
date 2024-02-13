69750906fd69fa0946b6f4d3b1cfeeb096bf7728
Shobhit710 <s.ansla710@gmail.com>
2024-02-13 18:43:00 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+import sys
+
+# Define your condition
+condition_is_false = False  # Replace this with your actual condition
+s1 = 'apple'
+s2 = 'apple'
+if not s1==s2 :
+    print("Condition is false, failing the script.")
+    sys.exit(1)
+else:
+    print("Condition is true, continuing with the script.")
